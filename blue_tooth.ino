ffe08f85acde454eecc4c90c215ca8232ae63e6f
AcerAspireE15 <maheshgupta067@gmail.com>
2017-06-21 18:07:02 +0530

    add bluetooth

---
+++ blue_tooth.ino
@@ -0,0 +1,27 @@
+int state = 0;
+void setup(){
+  
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Serial.begin(38400);
+  
+}
+
+void loop(){
+  
+  if(Serial.available() > 0)
+  {
+    state=Serial.read();}
+    
+    if(state == '0')
+    {
+      digitalWrite(13, LOW);
+      Serial.println("LED : OFF");
+      state=0;
+    }
+    else if( state == '1')
+    {
+      digitalWrite(13, HIGH);
+      Serial.println("LED : ON");
+      state=0;
+    }}
---
+++ bluetooth.ino
@@ -0,0 +1,33 @@
+char data=0;
+void setup()
+{
+ pinMode(11, OUTPUT);
+ Serial.begin(9600);
+ pinMode(10, OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop(){
+  if(Serial.available() > 0);
+  {
+    data=Serial.read();
+    Serial.print(data);
+    Serial.print("\n");
+    if(data=='1')
+ 
+   digitalWrite(11, HIGH);
+  else if(data=='0')
+  digitalWrite(11, LOW);
+  }
+   if(Serial.available() > 0);
+  {
+    data=Serial.read();
+    Serial.print(data);
+    Serial.print("\n");
+    if(data=='1')
+ 
+   digitalWrite(10, HIGH);
+  else if(data=='0')
+  digitalWrite(10, LOW);
+}
+}
