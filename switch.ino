772dbec4e457767d7455cb7cdedbf30e9e0fbae3
AcerAspireE15 <maheshgupta067@gmail.com>
2017-06-21 18:10:16 +0530

    add switch

---
+++ switch.ino
@@ -0,0 +1,23 @@
+
+
+int buttonState=0;
+
+void setup(){
+  
+  pinMode(13, OUTPUT);
+  pinMode(8, INPUT);
+  
+}
+
+void loop(){
+  
+  buttonState = digitalRead(8);
+  if(buttonState == HIGH){
+    
+  digitalWrite(13, HIGH);
+}
+else{
+  digitalWrite(13, LOW);
+  
+}
+}
